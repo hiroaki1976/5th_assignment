a071b2775e256e5c0bfcd465ce185ca28139b00f
hiroaki1976 <yokota2h@gmail.com>
2023-11-12 20:52:46 +0900

    20231112_HTML/CSS最低ライン完了

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import { getDatabase, ref, push, set, onChildAdded, remove, onChildRemoved }
+    from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
+// Your web app's Firebase configuration
+// 以下のfirebaseConfigは絶対githubにはアップロードしない！！
