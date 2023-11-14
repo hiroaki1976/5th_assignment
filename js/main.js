60b075def09e6098c5617df6359cb12a2b308294
hiroaki1976 <yokota2h@gmail.com>
2023-11-14 23:44:39 +0900

    20231114_終了時点

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
-import { getDatabase, ref, push, set, onChildAdded, remove, onChildRemoved }
+import { getDatabase, ref, push, set, onChildAdded, remove, onChildRemoved, update, onChildChanged }
     from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
 // Your web app's Firebase configuration
 // 以下のfirebaseConfigは絶対githubにはアップロードしない！！
@@ -11,15 +11,17 @@ const firebaseConfig = {
     const db = getDatabase(app); //RealtimeDBに接続
     const dbRef = ref(db, "chat"); //RealtimeDB内の"chat"を使う
 
+
 //データ登録(Click)
 $("#send").on("click", function () {
     const msg = {
         name: $("#name").val(),
         text: $("#text").val()
     }
-    const newPostRef = push(dbRef); //ユニークKEYを生成
-    set(newPostRef, msg);           //"chat"にユニークKEYをつけてオブジェクトデータを登録
+    const newPostRef = push(dbRef);//ユニークKEYを生成（これをつけないとデータが上書きされる）
+    set(newPostRef, msg);//"chat"にユニークKEYをつけてオブジェクトデータを登録
 });
+
 
 //データ登録(Enter)
 $("#text").on("keydown", function (e) {
@@ -29,27 +31,40 @@ $("#text").on("keydown", function (e) {
             name: $("#name").val(),
             text: $("#text").val()
         }
-        const newPostRef = push(dbRef); //ユニークKEYを生成
-        set(newPostRef, msg);           //"chat"にユニークKEYをつけてオブジェクトデータを登録
+        const newPostRef = push(dbRef);
+        set(newPostRef, msg);
     }
 });
 
+
 //最初にデータ取得＆onSnapshotでリアルタイムにデータを取得
 onChildAdded(dbRef, function (data) {
-    const msg = data.val();    //オブジェクトデータを取得し、変数msgに代入
-    const key = data.key;      //データのユニークキー（削除や更新に使用可能）
+    const msg = data.val();//オブジェクトデータを取得し、変数msgに代入
+    const key = data.key;//データのユニークキー（削除や更新に必須）
     //表示用テキスト・HTMLを作成
-    let h = '<p>';
-    h += msg.name;
-    h += '<br>';
-    h += msg.text;
-    h += '</p>';
-    $("#output").append(h); //#outputの最後に追加
+    let html = `
+        <div class="balloon_r ${key}">
+            <div class="faceicon">
+                <img src="#.jpg" alt="" >
+            </div>
+            <div class="says">
+                <p>${msg.name}</p><br>
+                <p>${msg.text}</p>
+                <span class="remove" data-key="${key}">🗑</span>
+            </div>
+        </div>
+    `
+    $("#output").append(html); //#outputの最後に追加
 });
 
-// $('.inputarea').on('click', '#send', function () {
-//     const iaItem = $(this).closest("div"); // クリックされたボタンに最も近い親 <li> 要素を取得
-//     const name = iaItem.find('input').text();
-//     const text = iaItem.find('textarea').text();
-//     name = $('#name').val();
-//     text = $('#text').val();
+
+// 個別のチャットの削除
+$('#output').on('click', '.remove', function (){
+    const key = $(this).attr('data-key');
+    const removeItem = ref(db, 'chat/'+key);
+    remove(removeItem);// Firebaseデータ削除関数
+});
+
+onChildRemoved(dbRef, function(data) {
+    $('.'+data.key).remove();// 対象を削除
+});
